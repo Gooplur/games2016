c793d0e09ad30f0d4d5e9ff8b1a97ce2aa0c78ac
Gooplur <Skye_lipson@yahoo.com>
2016-07-07 15:50:52 -0700

    tried to fix the invfix cheat

--- web/legendOfTheCrackBear.js
+++ web/legendOfTheCrackBear.js
@@ -922,8 +922,8 @@ function theLegend()
             }
             else if (cheatcode.toLowerCase() == "invfix")
             {
-                invScroll = 0;
-                player.inventoryPosition = 0;
+                invScroll -= 79;
+                player.inventoryPosition += 1;
             }
             else if (cheatcode.toLowerCase() == "serpytheserpent")
             {

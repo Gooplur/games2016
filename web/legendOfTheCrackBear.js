1b9038344ec21bd374caefd38dc701ed4e7abef1
Gooplur <Skye_lipson@yahoo.com>
2016-07-07 15:25:59 -0700

    tried to fix the invfix cheat

--- web/legendOfTheCrackBear.js
+++ web/legendOfTheCrackBear.js
@@ -922,7 +922,7 @@ function theLegend()
             }
             else if (cheatcode.toLowerCase() == "invfix")
             {
-                invScroll -= 79;
+                invScroll = 0;
             }
             else if (cheatcode.toLowerCase() == "serpytheserpent")
             {
